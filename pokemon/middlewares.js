77f7d78469cb7d5a76f9aeb4a363bcd11be4c9f0
zerob13 <zerob13@gmail.com>
2020-03-28 00:18:38 +0800

    bug fix

--- pokemon/middlewares.js
+++ pokemon/middlewares.js
@@ -1,8 +1,7 @@
-const _ = require('koa-route')
-
-exports.default = [
-  _.get('/num/:id', async (ctx, next) => {
-    ctx.body = `https://cn.portal-pokemon.com/play/pokedex/${id}`
-    await next()
-  })
-]
+const Router = require('koa-router')
+const _ = new Router()
+_.get('/num/:id', async (ctx, next) => {
+  ctx.body = `https://cn.portal-pokemon.com/play/pokedex/${ctx.params.id}`
+  await next()
+})
+exports.default = [_.routers(), _.allowedMethods()]
