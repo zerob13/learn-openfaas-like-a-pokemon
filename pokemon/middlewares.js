c94078ca859e3cd5af18902ea3c1b9cd72e427ba
zerob13 <zerob13@gmail.com>
2020-03-28 00:29:58 +0800

    fix import

--- pokemon/middlewares.js
+++ pokemon/middlewares.js
@@ -1,4 +1,4 @@
-const Router = require('koa-router')
+const Router = require('@koa/router')
 const _ = new Router()
 _.get('/num/:id', async (ctx, next) => {
   ctx.body = `https://cn.portal-pokemon.com/play/pokedex/${ctx.params.id}`
