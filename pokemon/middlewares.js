f8db98fa60ef57b35249d5032f5b8866debf942b
zerob13 <zerob13@gmail.com>
2020-03-28 00:38:20 +0800

    nice try

--- pokemon/middlewares.js
+++ pokemon/middlewares.js
@@ -1,7 +1,19 @@
 const Router = require('@koa/router')
 const _ = new Router()
 _.get('/num/:id', async (ctx, next) => {
-  ctx.body = `https://cn.portal-pokemon.com/play/pokedex/${ctx.params.id}`
+  let pokeid = ctx.params.id
+  if (!pokeid) {
+    pokeid = 001
+  }
+  if (pokeid.length == 1) {
+    pokeid = `00${pokeid}`
+  }
+  if (pokeid.length == 2) {
+    pokeid = `0${pokeid}`
+  }
+  const url = `https://cn.portal-pokemon.com/play/pokedex/${pokeid}`
+  ctx.status = 302
+  ctx.redirect(url)
   await next()
 })
 exports.default = [_.routes(), _.allowedMethods()]
